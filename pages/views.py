38398b3f401d824a2a8148c7bcce570ba085cc14
Manuela Caro Villada <104787569+ManuCarov@users.noreply.github.com>
2025-03-20 21:18:11 -0500

    home

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
+
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+class OptionsView(TemplateView):
+    template_name = 'vinyl/index.html'
